441e06795b5d22f694e2424c152a78e8fb1ad67e
gerardovillegas <villegasgerardo.j@gmail.com>
2020-01-22 09:38:56 -0500

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,13 +3,7 @@ import firebase from 'firebase/app';
 import firestore from 'firebase/firestore';
 
 var firebaseConfig = {
-	apiKey: 'AIzaSyBrrY2vjKJpPgx_p00NZYz8D92N6YrpBVE',
-	authDomain: 'vue-crud-d2cff.firebaseapp.com',
-	databaseURL: 'https://vue-crud-d2cff.firebaseio.com',
-	projectId: 'vue-crud-d2cff',
-	storageBucket: 'vue-crud-d2cff.appspot.com',
-	messagingSenderId: '546102954133',
-	appId: '1:546102954133:web:6f2dfa9982ab48c464b39c'
+	'Pegar creenciales de firebase'
 };
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
